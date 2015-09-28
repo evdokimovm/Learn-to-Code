9c6a103b37d1be56f2bac39bb803491250e56296
evdokimovm <tel89147925532@gmail.com>
2015-09-28 20:53:25 +1000

    Add Controller. Output Description.

--- Shaping Up with AngularJS/Level 2/Tabs Inside Out/app.js
+++ Shaping Up with AngularJS/Level 2/Tabs Inside Out/app.js
@@ -1,3 +1,25 @@
 (function() {
 	var app = angular.module('store', []);
+
+	app.controller('Controller', function(){
+		this.product = gem;
+	});
+
+	var gem = [
+	{
+		name: 'Dodecahedron',
+		price: 2.95,
+		description: 'Lorem ipsum Amet esse nulla Duis in eiusmod in enim sint elit sint sed incididunt dolore qui enim Excepteur aute ea nulla amet dolor nulla voluptate laboris eu aute qui proident est aute eiusmod reprehenderit fugiat minim quis cillum magna exercitation.',
+		canPurchase: true,
+		// soldOut: true,
+	},
+	{
+		name: 'Dodecahedron',
+		price: 2.95,
+		description: 'Rorem ipsum Amet esse nulla Duis in eiusmod in enim sint elit sint sed incididunt dolore qui enim Excepteur aute ea nulla amet dolor nulla voluptate laboris eu aute qui proident est aute eiusmod reprehenderit fugiat minim quis cillum magna exercitation.',
+		canPurchase: true,
+		// soldOut: true,
+	},
+	];
+
 })();
