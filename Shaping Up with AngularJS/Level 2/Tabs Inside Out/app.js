7150f50c74c2cfbb905d21a81cad63eb42758548
evdokimovm <tel89147925532@gmail.com>
2015-09-29 11:41:39 +1000

    Rewrite and Fix all

--- Shaping Up with AngularJS/Level 2/Tabs Inside Out/app.js
+++ Shaping Up with AngularJS/Level 2/Tabs Inside Out/app.js
@@ -5,22 +5,31 @@
 		this.product = gem;
 	});
 
-	app.controller('PanelController', function(){
-		
+	app.controller('PanelController', function() {
+		this.tab = 1;
+
+		this.selectTab = function(setTab) {
+			this.tab = setTab;
+		};
+
+		this.isSelected = function(checkTab) {
+			return this.tab === checkTab;
+		};
 	});
 
 	var gem = [
 	{
 		name: 'Dodecahedron',
 		price: 2.95,
-		description: 'Lorem ipsum Amet esse nulla Duis in eiusmod in enim sint elit sint sed incididunt dolore qui enim Excepteur aute ea nulla amet dolor nulla voluptate laboris eu aute qui proident est aute eiusmod reprehenderit fugiat minim quis cillum magna exercitation.',
+		description: 'Lorem ipsum Magna velit incididunt amet amet. Lorem ipsum Proident ut ex.',
 		canPurchase: true,
 		// soldOut: true,
 	},
+
 	{
 		name: 'Dodecahedron',
 		price: 2.95,
-		description: 'Rorem ipsum Amet esse nulla Duis in eiusmod in enim sint elit sint sed incididunt dolore qui enim Excepteur aute ea nulla amet dolor nulla voluptate laboris eu aute qui proident est aute eiusmod reprehenderit fugiat minim quis cillum magna exercitation.',
+		description: 'Lorem ipsum Magna velit incididunt amet amet.',
 		canPurchase: true,
 		// soldOut: true,
 	},
