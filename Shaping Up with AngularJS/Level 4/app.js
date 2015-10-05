b971a3306895e6f39a2900da56c35fa9abb0bf6b
evdokimovm <tel89147925532@gmail.com>
2015-10-05 16:48:14 +1000

    Level 4

---
+++ Shaping Up with AngularJS/Level 4/app.js
@@ -0,0 +1,57 @@
+(function() {
+	var app = angular.module('store', []);
+
+	app.controller('Controller', function(){
+		this.products = gem;
+	});
+
+	app.controller('PanelController', function() {
+		this.tab = 1;
+
+		this.selectTab = function(setTab) {
+			this.tab = setTab;
+		};
+
+		this.isSelected = function(checkTab) {
+			return this.tab === checkTab;
+		};
+	});
+
+	app.controller('ReviewController', function() {
+		this.review = {};
+
+		this.addReview = function(product) {
+			product.reviews.push(this.review);
+			this.review = {};
+		};
+	});
+
+	var gem = 
+	[
+			{
+				name: 'Awesome Multi-touch Keyboard',
+				price: 250.00,
+				description: "Lorem ipsum Duis ad amet. Lorem ipsum Aute qui in.",
+				images: 
+				[
+						"images/gem-02.gif",
+						"images/gem-05.gif",
+						"images/gem-09.gif"
+				],
+				reviews: 
+				[
+					{
+						stars: 5,
+						body: "I love this product!",
+						author: "joe@thomas.com"
+					},
+					{
+						stars: 1,
+						body: "This product sucks",
+						author: "tim@hater.com"
+					}
+				]
+			},
+	];
+
+})();
