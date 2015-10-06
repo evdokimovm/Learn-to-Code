80457ae3e97a65606c2f8a469b6451b2151ce207
evdokimovm <tel89147925532@gmail.com>
2015-10-06 14:36:25 +1000

    Document into modules

--- Shaping Up with AngularJS/Level 4/app.js
+++ Shaping Up with AngularJS/Level 4/app.js
@@ -33,6 +33,27 @@
 			templateUrl: 'product-title.html'
 		};
 	});
+
+	app.directive('productPanels', function(){
+		return {
+			restrict: 'E',
+			templateUrl: 'product-panels.html'
+		};
+	});
+
+	app.directive('productGallery', function(){
+		return {
+			restrict: 'E',
+			templateUrl: 'product-gallery.html'
+		};
+	});
+
+	app.directive('productPanels1', function(){
+		return {
+			restrict: 'E',
+			templateUrl: 'product-panels1.html'
+		};
+	});
 	
 	var gem = 
 	[
