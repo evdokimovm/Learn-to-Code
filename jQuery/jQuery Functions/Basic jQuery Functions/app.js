4115ede69dcfeb659eccc63a0c6c9a2324084b25
evdokimovm <tel89147925532@gmail.com>
2015-10-09 17:43:19 +1000

    jQuery Functions. Basic jQuery Functions.

---
+++ jQuery/jQuery Functions/Basic jQuery Functions/app.js
@@ -0,0 +1,12 @@
+// $(document).ready(function() {
+//     $('button').click(function() {
+//         $('.vanish').fadeOut();
+//     });
+// });
+
+
+$(document).ready(function() {
+	$('div').click(function() {
+		$(this).fadeOut('slow');
+	});
+});
