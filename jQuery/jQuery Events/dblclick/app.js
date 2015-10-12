96b38a47655e7f726c34503429eba003bcb91b94
evdokimovm <tel89147925532@gmail.com>
2015-10-12 20:56:20 +1000

    jQuery Events. click and dblclick

---
+++ jQuery/jQuery Events/dblclick/app.js
@@ -0,0 +1,13 @@
+// $(document).ready(function(){
+// 	$('div').click(function(){
+// 			$(this).fadeOut('fast');
+// 			$('div').addClass('red');
+// 	});
+// });
+
+$(document).ready(function(){
+	$('div').dblclick(function(){
+			$(this).fadeOut('fast');
+			$('div').addClass('red');
+	});
+});
